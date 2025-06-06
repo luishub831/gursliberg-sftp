5c50bc4396b067d35dcb3c4315a58e59056509ce
LuisHub831 <work.group831@gmail.com>
2025-06-06 13:16:26 +0200

    updated formatter

--- formatter.js
+++ formatter.js
@@ -20,10 +20,10 @@ module.exports = function formatOrder(order) {
     pad(order.email, 100),   //"FN1_DelCustEmail(100)" 
     pad(order.phone || '', 12),     //"FN1_DelCustMobil(12)" 
     pad(order.total_weight, 12),                    // FN1_TotalWeight(12)
-    pad(order.app_id, 20),                    // FN1_ShopID
+    pad('8999869', 20),                    // FN1_ShopID
     pad('N/A', 12),                    // FN1_DeliveryMode
     pad('N/A', 48),                    // FN1_PickupPoint
-    pad('N/A', 10),                    // FN1_ClubID
+    pad('NGBF', 10),                    // FN1_ClubID
     pad('0', 1),                    // FN1_CustType (0 = private)
     pad('0', 1),                    // FN1_AddrType (0 = street)
   ];
@@ -32,13 +32,13 @@ module.exports = function formatOrder(order) {
     `FN2`,                                                  // FN2_RecType (fixed value "FN2")
     pad(order.order_number, 20),                      // FN2_OrderNo (Invoice/order number)
     pad(item.product_id || '', 8),                                 // FN2_ArticleNo (internal item number)
-    pad(item.sku || '', 21),                                // FN2_ItemNumber (EAN)
+    // pad(item.sku || '', 21),                                // FN2_ItemNumber (EAN)
     pad(item.title || '', 80),                              // FN2_ItemName (product name)
     pad(item.quantity, 9),                            // FN2_DelNumber (number of items to deliver)
     pad(index + 1, 3),                                // FN2_LineNumber (line number, starting at 1)
     pad('1', 1),                                             // FN2_PickableItem (1 = pickable, or blank)
-    pad('OWNER', 6),                                        // FN2_ItemOwner (you can customize this)
-    pad(item.price, 8),                               // FN2_Price (sales price)
+    pad('93311', 6),                                        // FN2_ItemOwner (you can customize this)
+    // pad(item.price, 8),                               // FN2_Price (sales price)
     pad('N/A', 20),                                            // FN2_LineRef (line reference, optional/blank)
   ].join('')).join('\n');
 
