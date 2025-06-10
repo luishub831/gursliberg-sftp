4359991a3cc6fec825f56093f186710ce7f9b73c
luishub831 <work.group831@gmail.com>
2025-06-10 16:15:25 +0200

    update format

--- formatter.js
+++ formatter.js
@@ -21,8 +21,8 @@ module.exports = function formatOrder(order) {
     pad(order.phone || '', 12),     //"FN1_DelCustMobil(12)" 
     pad(order.total_weight, 12),                    // FN1_TotalWeight(12)
     pad('8999869', 20),                    // FN1_ShopID
-    pad('N/A', 12),                    // FN1_DeliveryMode
-    pad('N/A', 48),                    // FN1_PickupPoint
+    pad('', 1),                    // FN1_DeliveryMode
+    pad('', 1),                    // FN1_PickupPoint
     pad('NGBF', 10),                    // FN1_ClubID
     pad('0', 1),                    // FN1_CustType (0 = private)
     pad('0', 1),                    // FN1_AddrType (0 = street)
@@ -32,6 +32,7 @@ module.exports = function formatOrder(order) {
     `FN2`,                                                  // FN2_RecType (fixed value "FN2")
     pad(order.order_number, 20),                      // FN2_OrderNo (Invoice/order number)
     // pad(item.product_id || '', 8),                                 // FN2_ArticleNo (internal item number)
+    pad('', 8),                                 // FN2_ArticleNo (internal item number)
     pad(item.sku || '', 21),                                // FN2_ItemNumber (EAN)
     pad(item.title || '', 80),                              // FN2_ItemName (product name)
     pad(item.quantity, 9),                            // FN2_DelNumber (number of items to deliver)
@@ -39,7 +40,7 @@ module.exports = function formatOrder(order) {
     pad('1', 1),                                             // FN2_PickableItem (1 = pickable, or blank)
     pad('93311', 6),                                        // FN2_ItemOwner (you can customize this)
     // pad(item.price, 8),                               // FN2_Price (sales price)
-    pad('N/A', 20),                                            // FN2_LineRef (line reference, optional/blank)
+    pad('', 1),                                            // FN2_LineRef (line reference, optional/blank)
   ].join('')).join('\n');
 
   return `${FN1.join('')}\n${FN2}`;
